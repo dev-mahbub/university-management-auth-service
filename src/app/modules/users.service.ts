fb2b47fa3bdc3960847ef6f5078592a0951a126e
dev-mahbub <mahbub.devs@gmail.com>
2025-05-06 16:31:47 +0600

    [DONE] user service created

--- src/app/modules/users.service.ts
+++ src/app/modules/users.service.ts
@@ -2,8 +2,13 @@ import { Error } from 'mongoose'
 import { IUser } from './users.interface'
 import { User } from './users.model'
 import config from '../../config/index'
+import { generatedUserId } from './user.utils'
 
 const createUsers = async (user: IUser): Promise<IUser | null> => {
+  //auto generated incereamental id
+  const id = await generatedUserId()
+  user.id = id
+  //default password
   if (!user.password) {
     user.password = config.default_user_pass as string
   }
