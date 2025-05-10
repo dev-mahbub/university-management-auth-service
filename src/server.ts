d1dc40fbf97322c7f9c47c0e682125aa9f6214cb
dev-mahbub <mahbub.devs@gmail.com>
2025-05-10 12:27:20 +0600

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 import config from './config/index'
 import app from './app'
+import { logger, errLogger } from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Database connected successfully`)
+    logger.info(`Database connected successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+      logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log(`Failed to connect database`, err)
+    errLogger.error(`Failed to connect database`, err)
   }
 }
 
---
+++ src/shared/logger.ts
@@ -0,0 +1,56 @@
+import { createLogger, format, transports } from 'winston'
+const { combine, timestamp, label, printf } = format
+import DailyRotateFile from 'winston-daily-rotate-file'
+import path from 'path'
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp as string)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${date.toDateString()} ${hour} : ${minutes} : ${seconds} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(label({ label: 'MH' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'MH-%DATE%-success.log',
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+const errLogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'MH' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'MH-%DATE%-error.log',
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+export { logger, errLogger }
