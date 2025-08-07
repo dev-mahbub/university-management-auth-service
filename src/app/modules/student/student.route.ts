da58e79ca0fa12b837fce91366ff2f7663434116
dev-mahbub <mahbub.devs@gmail.com>
2025-08-07 23:11:34 +0600

    updated student

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { StudentController } from './student.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { StudentValidation } from './student.validation';
 const router = express.Router();
 
 router.get('/', StudentController.getAllStudents);
 router.get('/:id', StudentController.getSingleStudent);
-router.patch('/:id', StudentController.updateStudent);
-router.patch('/:id', StudentController.deleteStudent);
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.updateStudentZodSchema),
+  StudentController.updateStudent,
+);
+router.delete('/:id', StudentController.deleteStudent);
 
 export const StudentRoutes = router;
